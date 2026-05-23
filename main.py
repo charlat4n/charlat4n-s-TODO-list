f40e3d0a18758820002027884c0a302a1a5413ac
charlat4n <charlat4n>
2026-05-23 20:30:01 +0300

    Added file saving

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 #Lisada taske
 #Kuvada taske
 #Eemaldada taske
-#67
+import json
 
 def main():
 
-    tasks = []
+    file_constant = "tasks.json"
 
     def view_tasks():
         if len(tasks) == 0:
@@ -21,9 +21,53 @@ def main():
                 print(f"{i+1}. {task}")
             print("-------------\n")
 
+    def add_task():
+        task = input("Enter a task: ").strip()
+        if task:
+            tasks.append(task)
+            print("Task added")
+        else:
+            print("Enter a valid task")
 
+    def remove_task():
+        if len(tasks) == 0:
+            print("There are no tasks to remove")
+            return
+
+        view_tasks()
+        try:
+
+            removed_task = int(input("Enter the task you would like to remove: "))
+                    
+            if 1 <= removed_task <= len(tasks):
+                removed = tasks.pop(removed_task - 1)
+                print(f"Task '{removed}' removed.")
+
+            else:
+                print("Such a task doesn't exist")
+
+        except ValueError:
+            print("Entered input isn't valid")
+   
+    def load_tasks():
+        try:
+            with open(file_constant) as file:
+                opened = json.load(file)
+                return opened
+            
+        except FileNotFoundError:
+            tasks = []
+            return tasks
+
+    def save_tasks():
+        with open(file_constant, "w") as file:
+            json.dump(tasks, file, indent=4)
+
+    tasks = load_tasks()
 
     while True:
+
+
         print("\n-------------")
         print("1. View tasks")
         print("2. Add tasks")
@@ -37,34 +81,16 @@ def main():
             view_tasks()
 
         elif choice == "2":
-            task = input("Enter a task: ").strip()
-            if task:
-                tasks.append(task)
-                print("Task added")
-            else:
-                print("Enter a valid task")
+            add_task()
+            save_tasks()
 
         elif choice == "3":
-            if len(tasks) == 0:
-                print("There are no tasks to remove")
-                continue
-
-            view_tasks()
-            try:
-
-                removed_task = int(input("Enter the task you would like to remove: "))
-                    
-                if 1 <= removed_task <= len(tasks):
-                    removed = tasks.pop(removed_task - 1)
-                    print(f"Task '{removed}' removed.")
-
-                else:
-                    print("Such a task doesn't exist")
-
-            except ValueError:
-                print("Entered input isn't valid")
+            remove_task()
+            save_tasks()
 
         elif choice == "4":
+            save_tasks()
+            print("\nTasks saved.")
             print("\nGoodbye!")
             break
         
